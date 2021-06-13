e045938fae37342487280cebb51055e627cb2d8f
crabster15 <j715crab@gmail.com>
2021-06-12 22:08:46 -0500

    consolidated gui into front.tcl, created a FileHandler library and worked to fix binding the login to The FileHandling functionality

--- FileHandler.tcl
+++ FileHandler.tcl
@@ -1,26 +1,24 @@
-package provide FileHandler $FileHandler::version
-package require Tcl 8.5
-package require sha256
-
 namespace eval ::FileHandler {
- 
   
   namespace export check_password_attempt load_file check_null_file check_hash hash write_file parse_entries
-  set version 1.0
-  set hashedpass ""
+  variable hashedpass ""
+  variable passmap 
 
  
 }
-proc check_password_attempt { text } {
-    if { check_null_file == "true"} {
-        write_file accountpass text
-        return 
-    }
-    else {
-        load_file placehodler
-        set storedhash [dict get $passmap $accountpass]
+proc FileHandler::check_password_attempt { text } {
+    set filetruth [check_null_file]
+    if { $filetruth == "true"} {
         set hashtext [hash $text]
-        check_hash hashtext $storedhash $
+        write_file accountpass $hashtext
+    } else {
+        load_file placehodler
+        set storedhash [dict get $FileHandler::passmap accountpass]
+        set hashtext [hash $text]
+        set hashtruth [check_hash $hashtext $storedhash]
+        if { $filetruth == "true"} {
+            
+        }
     }
 
 }
@@ -30,16 +28,14 @@ proc FileHandler::load_file { action } {
     set file_data [read $fp]
     close $fp
     set lines [split $file_data "\n"]
-    puts $lines
-    if { action == "del case"} {
+    if { $action == "del case"} {
         set fp [open "hashes.txt" w]
         set $lines [ del_entry ] 
         puts $fp lines
         close $fp
         
-    }
-    else {
-        parse_entries $line
+    } else {
+        parse_entries $lines
     }
 }
 
@@ -48,24 +44,23 @@ proc FileHandler::check_null_file {} {
     set fp [open "hashes.txt" r]
     set file_data [read $fp]
     close $fp
-    if { file_data == " " } {
+    if { $file_data == "" } {
         return true
-    }
-    else {
+    } else {
         return false
     }
 }
 
-proc FileHandler::parse_entries { line } {
+proc FileHandler::parse_entries { lines } {
     global passmap
-    foreach entry $line {
-        set passes [split [ lindex $lines $i] ":"]
-        dict set passmap  [lindex $passes 0]  [lindex $passes 0]
+    for { set a 0}  {$a < [llength $lines]} {incr a} {
+        set passes [split [ lindex $lines $a] ":"]
+        dict set FileHandler::passmap  [lindex $passes 0]  [lindex $passes 0]
     }
 
 }
 
-proc FileHandelr::del_entry { lines case } {
+proc FileHandler::del_entry { lines case } {
     set foundIndex [lsearch $lines $case]
     # replace with nothing
     lreplace $lines foundIndex foundIndex
@@ -82,8 +77,7 @@ proc FileHandler::hash {text} {
 proc FileHandler::check_hash {hash text} {
     if { [string equal $hash $text] == 1} {
         return "false"
-    }
-    else {
+    } else {
         return "true"
     }
 
@@ -91,13 +85,35 @@ proc FileHandler::check_hash {hash text} {
 
 proc FileHandler::write_file {case pass} {
     set fp [open "hashes.txt" a]
-    set hashedpass [hash pass]
-    puts $fp $case
-    puts $fp :
-    puts $fp $hashedpass
+    puts $fp "$case:$pass"
     close $fp
 }
 
+# proc FileHandler::check_login {text} {
+#     set fileValue [ check_null_file ]
+#     if {$fileValue == "true"} {
+#         set hashedtext [FileHandler::hash text]
+#         FileHandler::write_file passlogin $hashedtext
+#     }  
+#     if {$fileValue == "false"} {
+#         load_file null 
+#         if { [dict exists $FileHandler::passmap passlogin] == 0} {
+#             puts killyourself
+#         } else {
+#             puts nibber
+#             set storedhash [dict get $FileHandler::passmap passlogin]
+#             set $FileHandler::hashedpass [hash $text]
+#             set checkPassVal [FileHandler::check_hash $storedhash $FileHandler::passmap]
+#             if {checkPassVal == "true"} {
+#                 puts "hi"
+#         }
+#         }
+#     }
+
+# }
+
+namespace eval ::FileHandler { variable version 1.0 }
+
 package provide FileHandler $FileHandler::version
-package require Tcl 8.5
+package require Tcl 8.5-
 package require sha256
--- front.tcl
+++ front.tcl
@@ -1,3 +1,7 @@
+lappend auto_path .
+package require  FileHandler 
+package require Tk
+
 #set demensions of gui window
 
 set width 300
@@ -6,36 +10,97 @@ set height 300
 set x [expr { ( [winfo vrootwidth  .] - $width  ) / 2 }]
 set y [expr { ( [winfo vrootheight .] - $height ) / 2 }]
 
+set password ""
+
+tk::toplevel .front
 #title gui and set geometry
+wm title .front "PassWordManager" 
+wm geometry .front  ${width}x${height}+${x}+${y}
+
+frame .front.
+pack .front. -fill both -expand 1
+
+ttk::label .front..tltlb -text PassWordManager
+pack .front..tltlb -pady 5 -anchor center -side top
+
+ttk::label .front..usrlb -text username
+pack .front..usrlb -anchor center -side top
+
+ttk::button .front..cb -text LOGIN -default active -command { [FileHandler::check_password_attempt $password ] }
+pack .front..cb -padx 5 -pady 5 -side bottom -anchor se
+bind .front. <Return> ".action invoke"
+
+
+ttk::entry .front..usrnm -textvariable username 
+pack .front..usrnm -anchor center -side top -pady 5
+
+
+ttk::label .front..paslb -text password
+pack .front..paslb -anchor center -side top
+
+
+tk::entry .front..passwd -textvariable password -show "*"
+pack .front..passwd -anchor center -side top 
+
+ttk::button .front..rb -text RESET_PASSWORD
+pack .front..rb -anchor center -side bottom -pady 5
+
+###set up main manager window
+
 wm title . "PassWordManager" 
 wm geometry . ${width}x${height}+${x}+${y}
 
+ttk::frame .fr -width $width -height $height
 
-frame .fr
-pack .fr -fill both -expand 1
+ttk::combobox .fr.kcase -textvariable kcase
+.fr.kcase configure -values [list ]
+bind .fr.kcase <<ComboboxSelected>> { script }
+.fr.kcase state readonly
 
-ttk::label .fr.tltlb -text PassWordManager
-pack .fr.tltlb -pady 5 -anchor center -side top
+ttk::entry .fr.newcase -textvariable newcase
 
-ttk::label .fr.usrlb -text username
-pack .fr.usrlb -anchor center -side top
+ttk::entry .fr.newpass -textvariable newpass -show "*"
+ttk::entry .fr.kpass -textvariable knownpass -show "*"
 
-ttk::button .fr.cb -text LOGIN -default active -command myaction
-pack .fr.cb -padx 5 -pady 5 -side bottom -anchor se
-bind .fr <Return> ".action invoke"
+ttk::button .fr.cb -text RAND_PASS
+ttk::button .fr.del -text Delete
+
+ttk::label .fr.kcaselb -text Website/Use
+ttk::label .fr.passlb -text "Known PassWord"
+ttk::label .fr.ncaselb -text "New PassWord Website/Use"
+ttk::label .fr.npasslb -text "New PassWord"
+
+ttk::label .fr.empty1 -text " " 
+ttk::label .fr.empty2 -text  " " 
+ttk::label .fr.empty3 -text " "
 
 
-ttk::entry .fr.usrnm -textvariable username 
-pack .fr.usrnm -anchor center -side top -pady 5
+# binded functions
 
+grid .fr -column 0 -row 0
+grid .fr.cb -column 1 -row 6
+grid .fr.newcase -column 0 -row 5 -pady 10 -padx 10
+grid .fr.newpass -column 1 -row 5 -pady 10 -padx 10
+grid .fr.ncaselb -column 0 -row 4
+grid .fr.npasslb -column 1 -row 4
+grid .fr.empty1 -column 0 -row 3 -pady 50
+grid .fr.empty2 -column 1 -row 3 -pady 50
+grid .fr.empty3 -column 0 -row 2
+grid .fr.del -column 1 -row 2
+grid .fr.kcase -column 0 -row 1
+grid .fr.kpass -column 1 -row 1
+grid .fr.kcaselb -column 0 -row 0 -pady 10 -padx 10
+grid .fr.passlb -column 1 -row 0 -pady 10 -padx 10
 
-ttk::label .fr.paslb -text password
-pack .fr.paslb -anchor center -side top
+grid columnconfigure .fr 0 -weight 1
+grid columnconfigure .fr 0 -weight 1
+grid columnconfigure .fr 1 -weight 1
+grid rowconfigure .fr 0 -weight 10
+grid rowconfigure .fr 1 -weight 1
+grid rowconfigure .fr 2 -weight 10
+grid rowconfigure .fr 3 -weight 1
+grid rowconfigure .fr 4 -weight 10
 
-
-tk::entry .fr.passwd -textvariable password -show "*"
-pack .fr.passwd -anchor center -side top 
-
-ttk::button .fr.rb -text RESET_PASSWORD
-pack .fr.rb -anchor center -side bottom -pady 5
-
+wm geometry . 1x1+0+0
+wm overrideredirect . 1
+wm transient .
